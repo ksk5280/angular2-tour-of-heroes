f75a38864ba1692b491839f9ddaf315e56c9531b
Kimiko Kano <kskano@gmail.com>
2016-07-28 23:58:22 -0600

    Add HeroDetailComponent and route

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,8 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-hero-detail',
+  templateUrl: 'app/hero-detail.component.html',
   template: `
   <div *ngIf="hero">
     <h2>{{hero.name}} details!</h2>
@@ -12,10 +16,32 @@ import { Hero } from './hero';
       <input [(ngModel)]="hero.name" placeholder="name"/>
     </div>
   </div>
+  <button (click)="goBack()">Back</button>
   `
 })
 
-export class HeroDetailComponent {
-  @Input()
+export class HeroDetailComponent implements OnInit, OnDestroy {
   hero: Hero;
+  sub: any;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute) {
+    }
+
+  ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      let id = +params['id'];
+      this.heroService.getHero(id)
+        .then(hero => this.hero = hero);
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  goBack() {
+    window.history.back();
+  }
 }
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -5,6 +5,11 @@ import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
+  getHero(id: number) {
+    return this.getHeroes()
+      .then(heroes => heroes.find(hero => hero.id === id));
+  }
+  
   getHeroes() {
     return Promise.resolve(HEROES);
   }
